31ad040e1f4d27a4de1a3c64c3d67d7581b21726
Kozu <kozu.vr@gmail.com>
2019-01-10 00:04:49 +0900

    Update A06_BoothPrefabRule.cs
    Add check for change of components

--- VketBoothValidator/Assets/VketBoothValidator/Editor/Rules/A_PackagingFormat/A06_BoothPrefabRule.cs
+++ VketBoothValidator/Assets/VketBoothValidator/Editor/Rules/A_PackagingFormat/A06_BoothPrefabRule.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 using System;
 using System.IO;
 using System.Text.RegularExpressions;
+using System.Linq;
 
 namespace VketTools
 {
@@ -31,12 +32,14 @@ namespace VketTools
         {
             base.Validate();
             string[] guids = AssetDatabase.FindAssets("t:prefab", new[] { AssetDatabase.GetAssetPath(options.baseFolder) });
+            string prefabPath = "";
             int boothPrefabCount = 0;
             foreach (string guid in guids)
             {
                 string assetPath = AssetDatabase.GUIDToAssetPath(guid);
                 if (Path.GetFileNameWithoutExtension(assetPath) == options.baseFolder.name)
                 {
+                    prefabPath = assetPath;
                     boothPrefabCount++;
                 }
             }
@@ -51,8 +54,9 @@ namespace VketTools
                 PropertyModification[] modification = PrefabUtility.GetPropertyModifications(scencebooth);
                 if (scencebooth != null)
                 {
+                    GameObject prefab = (GameObject)AssetDatabase.LoadAssetAtPath(prefabPath, typeof(GameObject));
                     if (hasModification(scencebooth, modification) || hasAttachedChild(scencebooth) ||
-                        hasAddedComponent(scencebooth) || hasRemovedComponent(scencebooth))
+                        hasComponentChange(scencebooth, prefab))
                     {
                         AddResultLog("シーン内のブースはプレハブから構成が変更されています。プレハブを最新に作り直してください。");
                         result = Result.FAIL;
@@ -70,16 +74,64 @@ namespace VketTools
             }
             return SetResult(result);
         }
-
-        private bool hasRemovedComponent(GameObject scencebooth)
+        /// <summary>
+        /// GameObject間でコンポーネントの増減をチェックする。
+        /// 子オブジェクトに対して再帰的にチェックする。
+        /// true:コンポーネントに増減がある false:増減がない
+        /// </summary>
+        private bool hasComponentChange(GameObject scencebooth, GameObject prefab)
         {
-            throw new NotImplementedException();
+            if (compareComponents(scencebooth, prefab))
+            {
+                return true;
+            }
+            else
+            {
+                //全ゲームオブジェクトについて
+                for (int i = 0; i < scencebooth.transform.childCount; i++)
+                {
+                    if (hasComponentChange(scencebooth.transform.GetChild(i).gameObject, prefab.transform.GetChild(i).gameObject))
+                    {
+                        return true;
+                    }
+                }
+                //なければfalseを返す。
+                return false;
+            }
         }
 
-        private bool hasAddedComponent(GameObject scencebooth)
+        private bool compareComponents(GameObject go1, GameObject go2)
         {
-            throw new NotImplementedException();
+            //両方のコンポーネントをリスト化
+            List<Component> go1Component = new List<Component>(go1.GetComponents<Component>());
+            List<Component> go2Component = new List<Component>(go2.GetComponents<Component>());
+            //シーン内オブジェクト側の全コンポーネントについてプレハブ側を探索
+            foreach (Component cmp1 in go1Component)
+            {
+                //同名のコンポーネントを検索
+                Component[] cmps = go2Component.Where(data => data.GetType() == cmp1.GetType()).ToArray<Component>();
+                if (cmps.Length == 0)
+                {
+                    AddResultLog(string.Format("プレハブから追加されたコンポーネント:{0}", cmp1.GetType().Name));
+                    //見つからなければ追加コンポーネント検知としてtrueを返す
+                    return true;
+                }
+            }
+            foreach (Component cmp2 in go2Component)
+            {
+                //同名のコンポーネントを検索
+                Component[] cmps = go1Component.Where(data => data.GetType() == cmp2.GetType()).ToArray<Component>();
+                if (cmps.Length == 0)
+                {
+                    AddResultLog(string.Format("プレハブから削除されたコンポーネント:{0}", cmp2.GetType().Name));
+                    //見つからなければ削除コンポーネント検知としてtrueを返す
+                    return true;
+                }
+            }
+            //何も検知しなかったらfalse
+            return false;
         }
+
 
         //ブースにprefabの構成物以外の子オブジェクトが追加されているかチェックする。
         private bool hasAttachedChild(GameObject scencebooth)
